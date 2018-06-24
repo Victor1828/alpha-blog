4944aa0f4d35abbb3cf655cb0cd4bb9c9b3f9564
Victor Vega <victorvega1828@gmail.com>
2018-06-24 01:00:40 -0500

    creación de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_23_040617) do
+ActiveRecord::Schema.define(version: 2018_06_24_031825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_06_23_040617) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
